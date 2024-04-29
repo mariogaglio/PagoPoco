d2793af88618bc45ce0adc050767c6b9823f131b
mario.gaglio@itisetorricelli.it <mario.gaglio@itisetorricelli.it>
2024-04-29 18:17:44 +0200

    First commit and create project

---
+++ pagopoco/src/main/java/com/itisetorricelli/pagopoco/App.java
@@ -0,0 +1,13 @@
+package com.itisetorricelli.pagopoco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
